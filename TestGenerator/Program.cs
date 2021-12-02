0cdf0de380993c315497cd079fa64db99f4ad64d
Aliaksandr Varashylau <aliaksandr.varashylau_rft@thomsonreuters.com>
2021-12-02 22:37:14 +0300

    (╯°□°)╯︵ GITHUB... Just eat my files and shut up ༼ つ ◕_◕ ༽つ

---
+++ TestGenerator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TestsGeneratorLib;
+namespace TestGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputFiles = new List<string>();
+            inputFiles.Add(@"..\..\test.cs");
+            string outputPath = @"..\..\GeneratedTests";
+            FileGenerator fileGenerator = new FileGenerator();
+            fileGenerator.Generate(inputFiles, outputPath,2,2,2);
+            Console.WriteLine("Done");
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
